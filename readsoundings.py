4c8cecf97204fc509d7d3291e8a00afdad3d794d
Kelly Neely <kelly.neely@ttu.edu>
2014-10-13 23:52:55 -0500

    After the code olympics.

---
+++ readsoundings.py
@@ -0,0 +1,18 @@
+# readsoundings.py file 
+
+import numpy as np
+
+def parse_SPC(filename, skip_rows=6):
+	""" Returns a "record array" which is a special type
+	of array that has column names and row access 
+	built into it.  The column names and data types
+	are defined by the dtype object."""
+	dtype = [('p', float),			# pressure, mb
+			 ('z', float),			# altitude, m
+			 ('T', float),			# temperature, C
+			 ('Td', float),			# dewpoint, C
+			 ('wind_dir', float),	# wind direction, degrees
+			 ('wind_spd', float)	# wind speed, knots
+			 ]
+	data = np.genfromtxt(filename, dtype = dtype, skip_header=skip_rows, delimiter=',')
+	return data
