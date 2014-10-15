5300398dbebf7d4ef647dcc5e6dd513502e7f440
Kelly Neely <kelly.neely@ttu.edu>
2014-10-15 15:00:33 -0500

    DUN dun DONEEE.

--- skewt.py
+++ skewt.py
@@ -6,47 +6,58 @@ from matplotlib.ticker import FuncFormatter, Formatter
 from mpl_toolkits.axisartist.grid_helper_curvelinear import GridHelperCurveLinear 
 from readsoundings import parse_SPC
 
-#import os
-#print os.listdir("/Users/Kelly/Documents/TTU/CloudPhysics")
-path_to_file = "/Users/Kelly/Documents/TTU/CloudPhysics/14101212_OBS_PIT.txt"
+
+path_to_file = "/Users/Kelly/Documents/TTU/SkewT/CloudPhysics/14101212_OBS_PIT.txt"
 sounding_data = parse_SPC(path_to_file)
 
 # Extract temperature from the sounding  and find the lines
 # with physical data values
+
 snd_T = sounding_data['T']
 snd_P = sounding_data['p']
 snd_Td = sounding_data['Td']
+
 # all temperature values, degrees C, should be in this range.
+
 good_T = (snd_T > -100.0) & (snd_T < 60.0)
+
 # all pressure values, in mb, should be in this range.
-good_P = (snd_P > 99.0) & (snd_P < 1001.0)
+
+good_P = (snd_P > 0) & (snd_P <= 1000.0)
+
 # all dewpoint columns, degrees C, should be in this range.
+
 good_Td = (snd_Td > -200.0) & (snd_Td < 60.0)
+
+
 
 C_to_K = 273.15 
 skew_slope = 40
 
 
 def x_from_Tp(T,p):
-	""" Calculates x from equation 1. """
+	""" Calculates x from equation 1, using
+	T in Kelvin and p in mb. """
 	x = T - (skew_slope*np.log(p))
 	return x
 
 def y_from_p(p):
-	""" Calculates y from equation 2. """
+	""" Calculates y from equation 2, using
+	p in mb. """
 	y = -(np.log(p))
 	return y
 
 def p_from_y (y):
-	""" Calculates p from equation 1. """
+	""" Calculates p from equation 1, using
+	p in mb. """
 	p = (np.exp(-y))
 	return p 
 
 def T_from_xp (x,p):
-	""" Calculates t from x and p. """
+	""" Calculates temperature in Kelvin
+	from x and p, which is in mb. """
 	t = x + (skew_slope*(np.log(p)))
-	return 
-
+	return t
 
 def to_thermo(x,y):
 	""" Transform (x,y) coordinates to T in
@@ -66,8 +77,11 @@ def from_thermo(T_C,p):
 # for plotting using the x and y coordinate 
 # conversion functions.
 
+
 x_snd_Td, y_snd_p = from_thermo(snd_Td, snd_P)
 x_snd_T, y_snd_p = from_thermo(snd_T, snd_P)
+
+
 
 # values along the bottom and left edges
 p_bottom = 1050.0
@@ -76,13 +90,15 @@ T_min = -40 + C_to_K
 T_max = 50 + C_to_K
 
 x_min, y_min = from_thermo((T_min - C_to_K), p_bottom)
-x_max, y_max = from_thermo((T_max - C_to_K), p_top)
+
+y_max = y_from_p(p_top)
+x_max = x_from_Tp(T_max,p_bottom)
 
 
 p_levels = np.arange(1000, 150-50, -50)
 T_C_levels = np.arange(-80, 40+10, 10)				# T levels in deg Celsius
 T_levels = T_C_levels + C_to_K 						# T levels in K
-theta_levels = np.arange(-40, 100+10, 10) + C_to_K # Theta levels in K 
+theta_levels = np.arange(-40, 100+10, 10) + C_to_K  # Theta levels in K 
 theta_ep_levels = theta_levels.copy() 				# Theta ep levels in K
 mixing_ratios = np.asarray([.0004, .001, .002, .003, .005, .008, .012, .016, .020])
 													# Mixing ratio in kg/kg
@@ -93,8 +109,7 @@ y_p_levels = y_from_p(p_levels)
 y_all_p = y_from_p(p_all)
 x_T_levels = [x_from_Tp(Ti, p_all) for Ti in T_levels]
 x_thetas = [x_from_Tp(Bolton.theta_dry(theta_i, p_all),p_all) for theta_i in theta_levels]
-x_mixing_ratios = [x_from_Tp((Bolton.mixing_ratio_line(p_all,Bolton.sat_mixing_ratio(p_all, Ti))), p_all) for Ti in T_C_levels]
-
+x_mixing_ratios = [x_from_Tp((Bolton.mixing_ratio_line(p_all,MRi)+C_to_K),p_all) for MRi in mixing_ratios]
 mesh_T, mesh_p = np.meshgrid(
 	np.arange(-60.0, T_levels.max()-C_to_K+0.1, 0.1), p_all)
 theta_ep_mesh = Bolton.theta_ep_field(mesh_T, mesh_p)
@@ -137,5 +152,6 @@ def format_coord(x, y):
 ax.format_coord = format_coord
 ax.plot(x_snd_Td, y_snd_p, linewidth = 2, color = 'g')
 ax.plot(x_snd_T, y_snd_p, linewidth = 2, color = 'r')
+
 
 plt.show()
